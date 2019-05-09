70ae64ace09be2d3131b3ad209563b59e953b7c5
SteelpanKooij <44843993+SteelpanKooij@users.noreply.github.com>
2019-05-09 15:02:19 +0200

    XMLExporter beginning

--- src/main/java/nl/nerdygadgets/infrastructure/design/XMLExporter.java
+++ src/main/java/nl/nerdygadgets/infrastructure/design/XMLExporter.java
@@ -1,4 +1,42 @@
 package nl.nerdygadgets.infrastructure.design;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+
+
 public class XMLExporter {
+
+    static void XMLExporterr() {
+        try {
+            DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+            Document document = documentBuilder.newDocument();
+
+            // Root element
+            Element root = document.createElement("root");
+            document.appendChild(root);
+
+            // Component element
+            Element component = document.createElement("Component");
+            root.appendChild(component);
+
+
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
 }

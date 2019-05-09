74094a3a653f246e2f849d9068f6eb58d5009a74
djabir omar <djabir13@live.nl>
2019-05-09 14:07:38 +0200

    FX componenten toegevoegd en een ArrayList gemaakt van de componenten

---
+++ src/main/java/nl/nerdygadgets/infrastructure/components/DBloadbalancer.java
@@ -0,0 +1,4 @@
+package nl.nerdygadgets.infrastructure.components;
+
+public class DBloadbalancer {
+}
---
+++ src/main/java/nl/nerdygadgets/infrastructure/components/HAL9002W.java
@@ -0,0 +1,4 @@
+package nl.nerdygadgets.infrastructure.components;
+
+public class HAL9002W {
+}

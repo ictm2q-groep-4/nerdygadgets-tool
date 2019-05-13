4af8c6a4c2f3ae4e487c162d133f02f1729ade7e
djabir omar <djabir13@live.nl>
2019-05-13 13:37:30 +0200

    Made a database Connection and a method for getting the upTime

--- src/main/java/nl/nerdygadgets/main/NerdyGadgets.java
+++ src/main/java/nl/nerdygadgets/main/NerdyGadgets.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.stage.Stage;
+import nl.nerdygadgets.database.Database;
 import nl.nerdygadgets.pages.PageRegister;
 
 import java.io.IOException;
@@ -30,6 +32,9 @@ public class NerdyGadgets extends Application {
 
     public static void main(String[] args) {
         launch(args);
+        System.out.println(Database.getDatabaseInstance());
+
+
     }
 
     @Override

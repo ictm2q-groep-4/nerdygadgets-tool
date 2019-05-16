04e7625de4eaf5a9856911d290007b76de2f7d44
SteelpanKooij <44843993+SteelpanKooij@users.noreply.github.com>
2019-05-16 08:04:06 +0200

    Pushing working version

--- src/main/java/nl/nerdygadgets/pages/controllers/DesignerController.java
+++ src/main/java/nl/nerdygadgets/pages/controllers/DesignerController.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.input.*;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.*;
 import nl.nerdygadgets.infrastructure.Infrastructure;
 import nl.nerdygadgets.infrastructure.components.Component;
@@ -27,7 +28,6 @@ import java.io.IOException;
 
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-import org.w3c.dom.NodeList;
 
 import static javafx.application.Application.launch;
 
@@ -44,6 +44,8 @@ public class DesignerController extends GenericController {
     public AnchorPane componentLayout;
     @FXML
     public static Event transferEvent;
+
+    public boolean attributeOK;
 
 
     /**
@@ -69,7 +71,7 @@ public class DesignerController extends GenericController {
     @FXML
     private void handleDrop(DragEvent dragEvent) {
         Pane component = (Pane) getTransferEvent().getSource();
-        HostnameAlert hostnameInput = new HostnameAlert();
+        PopupMenu hostnameInput = new PopupMenu();
         String hostname = null;
 
         //Boolean to check if component is already in the layout
@@ -77,14 +79,20 @@ public class DesignerController extends GenericController {
 
         //If the component does not exist in the layout, it will create a new instance of it
         if (!componentLayout.getChildren().contains(component)) {
-            if (hostnameInput.display()) {
+            if (hostnameInput.displayHostname()) {
                 hostname = hostnameInput.getHostname();
-                component = addComponentAttributes(component, hostname);
+                component = copyAttributes(component, hostname);
                 componentLayout.getChildren().add(component);
             } else {
                 return;
             }
         } else {
+            if (attributeOK) {
+                //TODO Add component attribute labels method
+//                addComponentAttributesLabels(component);
+                attributeOK = false;
+            }
+
             existenceCheck = true;
         }
 
@@ -115,79 +123,32 @@ public class DesignerController extends GenericController {
         if (existenceCheck) {
             //Edits the coordinates of an existing component
             editComponentObject(component);
-        } else{
+        } else {
             addComponentToInfrastructure(component, hostname);
         }
 
     }
 
-    private void editComponentObject(Pane component){
+    /**
+     * @param component
+     */
+    private void editComponentObject(Pane component) {
         List<Component> components = Infrastructure.getCurrentInfrastructure().getComponents();
 
-        for(Component x:components){
+        for (Component x : components) {
             String hostname = getHostname(component);
-            if(x.getHostname().equals(hostname)){
-                x.setX((int)component.getLayoutX());
-                x.setY((int)component.getLayoutY());
+            if (x.getHostname().equals(hostname)) {
+                x.setX((int) component.getLayoutX());
+                x.setY((int) component.getLayoutY());
                 System.out.println("X and Y coordinates of host '" + hostname + "' are changed. X: " + x.getX() + " Y: " + x.getY());
             }
-        }
-    }
-
-    /**
-     * A class for the hostnameAlert box
-     */
-    class HostnameAlert {
-        private boolean isOk;
-        TextField hostnameField;
-
-        public void main(String[] args) {
-            launch(args);
-        }
-
-        public boolean display() {
-            Stage window = new Stage();
-            window.initStyle(StageStyle.UTILITY);
-
-            VBox hostnameDialog = null;
-            try {
-                hostnameDialog = FXMLLoader.load(getClass().getResource("/pages/components/HostnameAlert.fxml"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            hostnameField = (TextField) hostnameDialog.getChildren().get(1);
-            Label hostnameWarning = (Label) hostnameDialog.getChildren().get(2);
-            Button okButton = (Button) hostnameDialog.getChildren().get(3);
-
-            okButton.setOnAction(actionEvent -> {
-                if(hostnameField.getText().isEmpty()){
-                    hostnameWarning.setText("Geen hostnaam ingevuld!");
-                    hostnameWarning.setVisible(true);
-                    //TODO A check to see if the hostname is unique in the components list
-                }else{
-                    isOk = true;
-                    window.close();
-                }
-            });
-
-            window.initModality(Modality.APPLICATION_MODAL);
-            window.setTitle("Hostname");
-            window.setScene(new Scene(hostnameDialog));
-
-            window.showAndWait();
-
-            return isOk;
-        }
-
-        public String getHostname() {
-            return hostnameField.getText();
         }
     }
 
 
     /**
      * Adds a component to the infrastructure when it's placed in the layout.
+     *
      * @param component
      * @param hostname
      */
@@ -197,17 +158,11 @@ public class DesignerController extends GenericController {
             Component componentObject = createComponentObject(component, hostname);
             infraComponents.add(componentObject);
             System.out.println("Component added to infrastructure. X and Y coordinates of host '" + hostname + "' are: X: " + componentObject.getX() + " Y: " + componentObject.getY());
+
         } catch (NoSuchMethodException | ClassNotFoundException e) {
             System.out.println("Creating component failed");
             e.printStackTrace();
         }
-    }
-
-    public String getHostname(Pane component){
-        VBox componentBox = (VBox) component.getChildren().get(0);
-        Label labelType = (Label) componentBox.getChildren().get(1);
-
-        return labelType.getText();
     }
 
 
@@ -221,8 +176,7 @@ public class DesignerController extends GenericController {
      * @throws ClassNotFoundException
      */
     public Component createComponentObject(Pane component, String hostname) throws NoSuchMethodException, ClassNotFoundException {
-        VBox componentBox = (VBox) component.getChildren().get(0);
-        Label labelType = (Label) componentBox.getChildren().get(0);
+        Label labelType = (Label) component.getChildren().get(0);
 
         String type = labelType.getText();
         String fullClassPath = "nl.nerdygadgets.infrastructure.components." + type;
@@ -240,30 +194,30 @@ public class DesignerController extends GenericController {
     }
 
     /**
-     *
+     * Copies the attributes from the component out of componentlist, and places it into a draggable pane with the createDraggablePane method.
      *
      * @param component
      * @param hostname
      * @return Pane component
      */
-    private Pane addComponentAttributes(Pane component, String hostname) {
-        Pane draggablePane = createDraggablePane();
+    private Pane copyAttributes(Pane component, String hostname) {
+        Pane draggablePane = createDraggablePane(component);
         VBox attributes = new VBox();
 
-        Label title = (Label) component.getChildren().get(1);
-        Label copyTitle = new Label(title.getText());
+        //Copy icon to componentpane
+        Label typeCopy = (Label) component.getChildren().get(1);
+        Label componentType = new Label(typeCopy.getText());
+        componentType.setVisible(false);
 
-        //TODO Add styling to TextFields
+        Rectangle iconCopy = (Rectangle) component.getChildren().get(0);
+        Rectangle componentIcon = new Rectangle(iconCopy.getHeight(), iconCopy.getWidth());
+
         Label name = new Label(hostname);
-        TextField ipv4 = new TextField();
-        TextField ipv6 = new TextField();
 
-        attributes.getChildren().add(copyTitle);
-
+        attributes.getChildren().add(componentIcon);
         attributes.getChildren().add(name);
-        attributes.getChildren().add(ipv4);
-        attributes.getChildren().add(ipv6);
 
+        draggablePane.getChildren().add(componentType);
         draggablePane.getChildren().add(attributes);
 
         return draggablePane;
@@ -274,15 +228,21 @@ public class DesignerController extends GenericController {
      *
      * @return AnchorPane draggableComponent
      */
-    public AnchorPane createDraggablePane() {
+    public AnchorPane createDraggablePane(Pane component) {
         AnchorPane draggableComponent = new AnchorPane();
 
         draggableComponent.setOnDragDetected(mouseEvent -> {
             handleDragDetection(mouseEvent);
         });
 
+        PopupMenu attributeDialog = new PopupMenu();
+        draggableComponent.setOnMouseClicked(mouseEvent -> {
+            attributeOK = attributeDialog.displayAttributes(component);
+        });
+
         return draggableComponent;
     }
+
 
     /**
      * Handles the detection of dragging
@@ -318,21 +278,84 @@ public class DesignerController extends GenericController {
 
         File selectedDirectory = fileChooser.showSaveDialog(NerdyGadgets.getNerdyGadgets().getStage());
 
-
+        //TODO Check that the component has IPv4 for saving
         if (selectedDirectory != null && selectedDirectory.getName().endsWith(".xml")) {
             try {
                 Infrastructure.getCurrentInfrastructure().save(selectedDirectory.getAbsolutePath());
 
             } catch (Exception E) {
                 E.printStackTrace();
-                NerdyGadgets.showAlert("Er is een fout opgetregen!", "", Alert.AlertType.ERROR);
+                NerdyGadgets.showAlert("Er is een fout opgetreden!", "", Alert.AlertType.ERROR);
             }
         } else {
-            NerdyGadgets.showAlert("Er is een fout opgetregen!", "Geen XML bestand!", Alert.AlertType.ERROR);
+            NerdyGadgets.showAlert("Er is een fout opgetreden!", "Geen XML bestand!", Alert.AlertType.ERROR);
         }
     }
+//
+//    //TODO addComponentAttributeLabels
+//    private void addComponentAttributesLabels(Pane component) {
+//        VBox componentPane = (VBox) component.getChildren().get(1);
+//    }
 
-
+//    private String getComponentAttribute(String method, String hostname) {
+//        List<Component> components = Infrastructure.getCurrentInfrastructure().getComponents();
+//
+//
+//        for (Component x : components) {
+//            //TODO Find a way to identify component in loop
+////            if (x.getHostname().equals(hostname)) {
+//            if (method.equals("getHostname")) {
+//                return x.getHostname();
+//            }
+//            if (method.equals("getipv4")) {
+//                //TODO Fix null
+//                return String.valueOf(x.getIpv4());
+//            }
+//            if (method.equals("getipv6")) {
+//                //TODO Fix null
+//                return String.valueOf(x.getIpv6());
+//            }
+//            if (method.equals("getUser")) {
+//                return x.getUser();
+//            }
+//            if (method.equals("getPass")) {
+//                return x.getPass();
+//            }
+//        }
+//
+////        }
+//        return "";
+//    }
+//
+////    private void setComponentAttribute(String method, String attribute) {
+////        List<Component> components = Infrastructure.getCurrentInfrastructure().getComponents();
+////
+//////            if (x.getHostname().equals(hostname)) {
+////        if (method.equals("setHostname")) {
+////            System.out.print(x.getHostname() + "\t Hostname changed to: ");
+////            x.setHostname(attribute);
+////            System.out.println(x.getHostname());
+////        }
+////        if (method.equals("setipv4")) {
+////            x.setIpv4(attribute);
+////            System.out.println(x.getHostname() + "\t IPv4: " + x.getIpv4());
+////        }
+////        if (method.equals("setipv6")) {
+////
+////            x.setIpv6(attribute);
+////            System.out.println(x.getHostname() + "\t IPv6: " + x.getIpv6());
+////        }
+////        if (method.equals("setUser")) {
+////            x.setUser(attribute);
+////            System.out.println(x.getHostname() + "\t SSH Username: " + x.getUser());
+////        }
+////        if (method.equals("setPass")) {
+////            x.setPass(attribute);
+////            System.out.println(x.getHostname() + "\t SSH Password: " + x.getPass() + " (Don't tell anyone!)");
+////        }
+////    }
+//    // }
+//    //}
 
     public static Event getTransferEvent() {
         return transferEvent;
@@ -342,4 +365,115 @@ public class DesignerController extends GenericController {
         transferEvent = event;
     }
 
+    public String getHostname(Pane component) {
+        VBox componentBox = (VBox) component.getChildren().get(1);
+        Label labelType = (Label) componentBox.getChildren().get(1);
+
+        return labelType.getText();
+    }
+
+    /**
+     * A class for the hostnameAlert box
+     */
+    class PopupMenu {
+        private boolean isOk;
+        private ArrayList componentAttributes = new ArrayList();
+
+        TextField hostnameField;
+
+        public void main(String[] args) {
+            launch(args);
+        }
+
+        public boolean displayHostname() {
+            Stage window = new Stage();
+            window.initStyle(StageStyle.UTILITY);
+
+            VBox hostnameDialog = null;
+            try {
+                hostnameDialog = FXMLLoader.load(getClass().getResource("/pages/components/AttributePopup.fxml"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            hostnameField = (TextField) hostnameDialog.getChildren().get(1);
+
+            Label hostnameWarning = (Label) hostnameDialog.getChildren().get(10);
+            Button okButton = (Button) hostnameDialog.getChildren().get(11);
+            okButton.setDefaultButton(true);
+
+            okButton.setOnAction(actionEvent -> {
+                if (hostnameField.getText().isEmpty()) {
+                    System.out.println("No hostname!");
+                    hostnameWarning.setText("Geen hostnaam ingevuld!");
+                    hostnameWarning.setVisible(true);
+                    //TODO A check to see if the hostname is unique in the components list
+                } else {
+                    isOk = true;
+                    window.close();
+                }
+            });
+
+            window.initModality(Modality.APPLICATION_MODAL);
+            window.setTitle("Hostname");
+            window.setScene(new Scene(hostnameDialog));
+
+            window.showAndWait();
+
+            return isOk;
+        }
+
+//        private void componentAttributes(String hostname, String ipv4, String ipv6, String sshuser, String sshpass) {
+//            componentAttributes = new ArrayList();
+//
+//            componentAttributes.add(hostname);
+//            componentAttributes.add(ipv4);
+//            componentAttributes.add(ipv6);
+//            componentAttributes.add(sshuser);
+//            componentAttributes.add(sshpass);
+//
+//        }
+
+        public String getHostname() {
+            return hostnameField.getText();
+        }
+
+        private ArrayList transferComponentAttributes() {
+            return componentAttributes;
+        }
+
+        public boolean displayAttributes(Pane component) {
+            Stage window = new Stage();
+
+            VBox attributeDialog = null;
+
+            try {
+                attributeDialog = FXMLLoader.load(getClass().getResource("/pages/components/AttributePopup.fxml"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            hostnameField = (TextField) attributeDialog.getChildren().get(1);
+
+            //TODO Retrieve component attributes
+
+            Button okButton = (Button) attributeDialog.getChildren().get(10);
+
+            okButton.setOnAction(actionEvent -> {
+                if (hostnameField.getText().isEmpty()) {
+                    //TODO A check to see if the hostname is unique in the components list
+                    System.out.println("No hostname");
+                } else {
+                    isOk = true;
+                    window.close();
+                }
+            });
+
+            window.setScene(new Scene(attributeDialog));
+
+            window.showAndWait();
+
+            return isOk;
+        }
+    }
 }

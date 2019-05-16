9132c08e2c3f99fff4be1586f3c4980eae0edad5
Lucas Ouwens <lucasouwens@gmail.com>
2019-05-16 18:09:15 +0200

    Added a few tests for the infrastructure class

--- src/test/java/nl/nerdygadgets/infrastructure/InfrastructureTest.java
+++ src/test/java/nl/nerdygadgets/infrastructure/InfrastructureTest.java
@@ -1,8 +1,13 @@
 package nl.nerdygadgets.infrastructure;
 
+import nl.nerdygadgets.infrastructure.components.Component;
+import nl.nerdygadgets.infrastructure.components.ComponentType;
 import nl.nerdygadgets.main.Components;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.xml.transform.TransformerException;
+import java.io.FileNotFoundException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,22 +25,33 @@ class InfrastructureTest {
 
     @Test
     public void testAddComponent() {
+        final Component input = new Component("HAL9003DB", 99.99, 15, ComponentType.DATABASESERVER);
+
         assertAll(
-                () -> assertTrue(infrastructure.addComponent(Components.getComponent("HAL9003DB")), "Adding a valid component should return TRUE")
+                () -> assertTrue(infrastructure.addComponent(input), "Adding a valid component should return TRUE"),
+                () -> assertFalse(infrastructure.addComponent(input), "Adding a component already in the list should return FALSE")
         );
     }
 
     @Test
     public void testRemoveComponent() {
+
+        // prepare arraylist for tests
+        infrastructure.getComponents().add(new Component("HAL9003DB", 99.99, 15, ComponentType.DATABASESERVER));
+
+        final Component input = infrastructure.getComponents().get(0);
+
+        // Should return false when a component isn't in the list
         assertAll(
-                () -> assertTrue(true)
+                () -> assertTrue(infrastructure.removeComponent(input)),
+                () -> assertFalse(infrastructure.removeComponent(input))
         );
     }
 
     @Test
     public void testSave() {
         assertAll(
-                () -> assertTrue(true)
+                () -> assertDoesNotThrow(()->infrastructure.save("test.xml"), "File should be created without any error.")
         );
     }
 }

4aa739c0dc761737899492173e8b08d3271aab2a
Joris Vos <jorisvos037@gmail.com>
2019-05-16 11:29:41 +0200

    Changed constructors of components
    Some constructors had wrong prices and availability in it (components package)

--- src/main/java/nl/nerdygadgets/infrastructure/components/HAL9001W.java
+++ src/main/java/nl/nerdygadgets/infrastructure/components/HAL9001W.java
@@ -10,6 +10,6 @@ public class HAL9001W extends Component {
     }
 
     public HAL9001W (String hostname, int x, int y, String username, String password, String ipv4, String ipv6) {
-        super(hostname, 90, 5100, ComponentType.WEBSERVER, x, y, username, password, ipv4, ipv6);
+        super(hostname, 80, 2200, ComponentType.WEBSERVER, x, y, username, password, ipv4, ipv6);
     }
 }
--- src/main/java/nl/nerdygadgets/infrastructure/components/HAL9002W.java
+++ src/main/java/nl/nerdygadgets/infrastructure/components/HAL9002W.java
@@ -10,6 +10,6 @@ public class HAL9002W extends Component {
     }
 
     public HAL9002W (String hostname, int x, int y, String username, String password, String ipv4, String ipv6) {
-        super(hostname, 90, 5100, ComponentType.WEBSERVER, x, y, username, password, ipv4, ipv6);
+        super(hostname, 90, 3200, ComponentType.WEBSERVER, x, y, username, password, ipv4, ipv6);
     }
 }

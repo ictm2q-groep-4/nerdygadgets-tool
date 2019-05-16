e49bfce36f717456af6e238ba0e6839057e3a4b4
Joris Vos <jorisvos037@gmail.com>
2019-05-16 11:31:46 +0200

    Update HAL9003DB.java

--- src/main/java/nl/nerdygadgets/infrastructure/components/HAL9003DB.java
+++ src/main/java/nl/nerdygadgets/infrastructure/components/HAL9003DB.java
@@ -5,7 +5,9 @@ package nl.nerdygadgets.infrastructure.components;
  * @author Djabir Omar Mohamed
  */
 public class HAL9003DB extends Component {
-    public HAL9003DB (String hostname, int x, int y) { super(hostname, 98, 12200, ComponentType.DATABASESERVER, x ,y); }
+    public HAL9003DB (String hostname, int x, int y) {
+        super(hostname, 98, 12200, ComponentType.DATABASESERVER, x ,y);
+    }
 
     public HAL9003DB (String hostname, int x, int y, String username, String password, String ipv4, String ipv6) {
         super(hostname, 98, 12200, ComponentType.DATABASESERVER, x, y, username, password, ipv4, ipv6);
